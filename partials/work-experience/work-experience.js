1b2e763de408f5e4ebd1d510e556b5b79e3b5db3
Will Thomson <will.thomson@gmail.com>
2018-05-23 22:20:40 +0100

    Stub for Work Experience JS

---
+++ partials/work-experience/work-experience.js
@@ -0,0 +1,7 @@
+export default class WorkExperiencePartial {
+  constructor(config) {
+    this.config = config || {};
+  }
+
+  // TODO: Add functionality to adapt content based on importance value
+}
--- source/js/composite/home.js
+++ source/js/composite/home.js
@@ -1,3 +1,5 @@
 import HeroPartial from '../../../partials/hero/hero';
+import WorkExperiencePartial from '../../../partials/work-experience/work-experience';
 
 new HeroPartial({});
+new WorkExperiencePartial({});
