a3dbfd97e0042da4e96e24e2d2808f5cde54bb32
Will Thomson <will.thomson@gmail.com>
2018-05-25 16:28:00 +0100

    Started work on importance buttons

--- partials/hero/hero.js
+++ partials/hero/hero.js
@@ -1,5 +1,48 @@
 export default class HeroPartial {
   constructor(config) {
     this.config = config || {};
+
+    this.importanceButtonContainerClass = 'hero__importance-btns';
+    this.importanceButtonClass = 'hero__importance-btn';
+    this.importanceAttr = 'data-importance';
+
+    this.importanceValues = [
+      {
+        name: 'Small',
+        value: 1
+      }, {
+        name: 'Medium',
+        value: 2
+      }, {
+        name: 'Large',
+        value: 3
+      },
+    ];
+    this.init();
+  }
+
+  init() {
+    const buttonContainer = document.querySelector(`.${this.importanceButtonContainerClass}`);
+
+    // Create the buttons
+    this.importanceValues.forEach(importanceValue => {
+      buttonContainer.innerHTML += `<button class="${this.importanceButtonClass}" data-importance="${importanceValue.value}">${importanceValue.name}</button>`;
+    });
+
+    // Add click events
+    document.querySelectorAll(`.${this.importanceButtonClass}`).forEach(btn => {
+      btn.addEventListener('click', e => {
+        const importance = e.target.getAttribute(this.importanceAttr);
+        const bodyEl = document.querySelector('body');
+
+        // Remove all importance classnames from body
+        this.importanceValues.forEach(importanceValue => {
+          bodyEl.classList.remove(`importance-${importanceValue.value}`);
+        });
+
+        // Add the importance classname from the clicked button
+        bodyEl.classList.add(`importance-${importance}`);
+      });
+    });
   }
 }
