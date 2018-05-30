75b24f11df3a6cc43aeebd1dcea86d02d3de74a5
Will Thomson <will.thomson@gmail.com>
2018-05-30 22:21:35 +0100

    Added a limit number to hide older work experience

--- partials/work-experience/work-experience.js
+++ partials/work-experience/work-experience.js
@@ -1,5 +1,23 @@
 export default class WorkExperiencePartial {
   constructor(config) {
     this.config = config || {};
+    this.showMoreLinkClass = 'work-experience__show-more';
+    this.hiddenClass = 'work-experience__job--hidden';
+    this.init();
+  }
+
+  init() {
+    const showMoreLink =
+      document.querySelector(`.${ this.showMoreLinkClass }`);
+
+    showMoreLink.addEventListener('click', (clickEvent) => {
+      clickEvent.preventDefault();
+      const hiddenJobs = document.querySelectorAll(`.${ this.hiddenClass }`);
+      hiddenJobs.forEach((job) => {
+        job.classList.remove(this.hiddenClass);
+      });
+      showMoreLink.remove();
+    });
   }
 }
+
